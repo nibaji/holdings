431806d9a2d300080f7138437c9be8fbe61edd0d
Nidhun Balaji T R <nidhunbalaji@gmail.com>
2024-03-09 14:46:58 +0530

    delegate styles to respective folder

---
+++ src/styles/screens/app.styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const Style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "grey",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default Style;
